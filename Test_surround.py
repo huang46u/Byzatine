ec9fb23d0d90dc3461cc208a7c81392c3f460be1
huang46u <qijiahuang1@gmail.com>
2022-05-03 17:01:03 +0200

    create file for surround relation, update the demo

--- Test_surround.py
+++ Test_surround.py
@@ -1,108 +1,64 @@
 #%%
+sys.path.append('./enlacement')
+import imp
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
+from skimage.morphology import convex_hull_image, area_opening, binary_erosion,binary_opening
+import surround as su
 import math
-from skimage.measure import label,block_reduce
-from sympy import comp, false
-from skimage.morphology import convex_hull_image, binary_erosion,binary_dilation,binary_opening
 import tools as tl
-import imp
+import preprocess.Image_extract as ext
 import enlacement.enlacement as en
 imp.reload(tl)
+imp.reload(su)
+imp.reload(ext)
 #%%
-im1 = np.array(Image.open("..\Qijia\ds0\masks_machine\Tatish 1754 Andr? spatharocandidat . jpg copie A.png"))
-mask_machine = np.array(Image.open("..\Qijia\ds0\masks_machine\Tatish 1754 Andr? spatharocandidat . jpg copie A.png"))[:,:,0]
-plt.imshow(mask_machine)
-plt.show()
-plt.imshow(im1)
-#%%
-labeled_image = label(mask_machine)
-print(tl.unique_value(labeled_image))
-plt.imshow((labeled_image==1)*labeled_image)
-plt.show()
-plt.imshow((labeled_image==2)*labeled_image)
-plt.show()
-plt.imshow((labeled_image==3)*labeled_image)
-plt.show()
-plt.imshow((labeled_image==4)*labeled_image)
-plt.show()
+json_path = "../Qijia/ds0/ann/Tatish 1754 Andr¨¦ spatharocandidat . jpg copie A.jpg.json"
+Image_dict = ext.Extract_image_mask(json_path)
+print(Image_dict.keys())
 # %%
 # Extract differnt part of the image
-left_flower = (labeled_image==1)
-cross = (labeled_image==2)
-cross =  tl.down_sample(cross)
+left_flower = Image_dict["Fleuron_1"]
 left_flower = tl.down_sample(left_flower)
 #%%
-cross = (mask_machine==5)
-marche = (mask_machine==2)
-l_cross =  tl.down_sample(cross)
-l_marche = tl.down_sample(marche)
-#%%
-chull = convex_hull_image(left_flower)
-ero_chull = binary_erosion(chull)
-plt.imshow(chull,cmap = 'gray')
-plt.show()
-plt.imshow(ero_chull,cmap = 'gray')
-plt.show()
-plt.imshow(left_flower,cmap = 'gray')
-# %%
-flower_boundary = left_flower- binary_erosion(left_flower)*1
-hull_boundary = ero_chull-binary_erosion(ero_chull)*1
-selem = np.array([[1,1],[1,1]])
-db_Ndch = (flower_boundary - hull_boundary)
-db_Ndch = (db_Ndch>0)*1
-p = ero_chull-left_flower
-p = binary_opening(p,selem=selem)*1
-
-#%%
-plt.imshow(chull)
-plt.show()
-plt.imshow(flower_boundary)
-plt.show()
+hull = convex_hull_image(left_flower)
+hull_boundary = hull-binary_erosion(hull)*1
 plt.imshow(hull_boundary)
+region = (hull_boundary+left_flower)
+region = hull - region
+region = region * (region>0)
+plt.imshow(region, cmap = "gray")
 plt.show()
-plt.imshow(db_Ndch)
-plt.show()
-plt.imshow(p,cmap = 'gray')
-plt.show()
-#%%
 # %%
-membership = tl.surround(left_flower,p)
+membership = su.surround(left_flower,region)
 # %%
 plt.imshow(membership, cmap = 'gray')
 plt.show()
+print(np.max(membership))
+print(np.min(membership))
+print(np.mean(membership[np.where(membership!=0)]))
 # %%
-mask_machine = np.array(Image.open("..\Qijia\ds0\masks_machine\Tatish 2805 AA 2016 copie.png"))[:,:,0]
-plt.imshow(mask_machine)
-plt.show()
-
+json_path = "../Qijia/ds0/ann/Tatish 2805 AA 2016 copie.jpg.json"
+Image_dict = ext.Extract_image_mask(json_path)
+print(Image_dict.keys())
 # %%
-labeled_image = label(mask_machine)
-print(tl.unique_value(labeled_image))
-plt.imshow((labeled_image==1)*labeled_image)
+round = Image_dict["Grenetis_0"]
+round = tl.down_sample(round)
+plt.imshow(round,cmap ='gray')
 plt.show()
-plt.imshow((labeled_image==2)*labeled_image)
-plt.show()
-plt.imshow((labeled_image==3)*labeled_image)
-plt.show()
-plt.imshow((labeled_image==4)*labeled_image)
-plt.show()
-
-# %%
-round = (labeled_image==1)*labeled_image
-round = block_reduce(round,block_size=(16,16))*1
+np.max(round)
+#%%
 chull = convex_hull_image(round)
 region = np.logical_xor(chull,round)
 region = binary_opening(region)*1
 plt.imshow(region,cmap = 'gray')
 # %%
-membership = tl.surround(round,region,bin = 120)
-# %%
-plt.imshow(round,cmap="gray")
-plt.show()
+membership = su.surround(round,region,n_dir = 120)
 plt.imshow(membership, cmap = 'gray')
 plt.show()
+print(np.max(membership))
+print(np.min(membership))
+print(np.mean(membership[np.where(membership!=0)]))
+
 # %%
-tl.debug_surround(round,region)
-#%%
---
+++ surround.py
@@ -0,0 +1,72 @@
+import math 
+import numpy as np
+import tools as tl
+def f(x):
+    if(x>=0.5*math.pi): return (x-0.5*math.pi)/(1.5*math.pi)
+    if(x<0.5*math.pi): return 0    
+            
+def mu_distance(dist, d1, d2):
+    if(dist <= d1): return 1
+    if(dist <= d2 and dist > d1): return (d2-dist)/(d2-d1)
+    else: return 0
+
+def get_intersect_coord(object,line):
+    if(object[line].sum()==0): return False, (-1,-1)
+    else:
+        cut= object[line] != 0
+        cut_ends = np.where(np.diff(cut) == True)
+        rr,cc = line
+        return True, (rr[cut_ends[0],cc[cut_ends[0]]]) 
+    
+def point_surround_angle_only(object, point, n_dir = 120):
+    """
+    Compute the surround fuzziness with angular coverage criteria only
+    
+    Args:
+        object: The region will be considered
+        point: The coordinate where we perform the computation 
+        n_dir: step size
+    """
+    intersect = np.zeros(n_dir)
+    angles = np.arange(0,2,2/n_dir)
+    for i in range(n_dir):
+        coord = tl.bresenham_ray(object,point,angles[i]*math.pi)
+        intersect[i]=(object[coord].sum()!=0)
+    percent = intersect.sum()/n_dir
+    return f(percent*2*math.pi)
+
+def point_surround_angle_and_distance(object, point, n_dir = 120):
+    """
+    Compute the surround fuzziness with angular coverage criteria and 
+        distance criteria
+    
+    Args:
+        object: The region will be considered
+        point: The coordinate where we perform the computation 
+        bin: step size
+    """
+    membership= 0
+    for i in np.arange(0,2,2/n_dir):
+        line = tl.bresenham_ray(object,point,i*math.pi)
+        intersect, inter_coord= get_intersect_coord(object, line)
+        length = 0 
+        if(intersect):
+            x1,y1 = point
+            x2,y2 = inter_coord
+            length = math.sqrt((x2-x1)**2+(y2-y1)**2)
+        membership += mu_distance(length)    
+    return membership
+
+def surround(object, candidate_region, mode= "angle", n_dir = 120):
+    coord = np.argwhere(candidate_region)
+    membership_grade = np.zeros(object.shape)
+    for i in coord:
+        if(mode == "angle"):
+            membership_grade[i[0],i[1]]= point_surround_angle_only(
+                object,i,n_dir = n_dir)
+        elif(mode == "distance"):
+            membership_grade[i[0],i[1]] = point_surround_angle_and_distance(
+                object, i, bin = n_dir)
+        else:
+            raise("mode can only be 'anlge' or 'distance'")
+    return membership_grade
